5c70c7b1095cdd6885df962f23d862ac095292b8
Riya Joshi <tanujajoshi2006@gmail.com>
2024-12-18 13:15:47 +0530

    Add files via upload

---
+++ Experiment_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float a,b,add,subtract,multiply,divide;
+    cout<<"Enter first number : ";
+    cin>>a;
+    cout<<"Enter second number : ";
+    cin>>b;
+    add=a+b;
+    subtract=a-b;
+    multiply=a*b;
+    divide=a/b;
+    cout<<"Sum is : "<<add<<"\n";
+    cout<<"substract is : "<<subtract<<"\n";
+    cout<<"Mutiplication is : "<<multiply<<"\n";
+    cout<<"Division  is : "<<divide<<"\n";
+}
